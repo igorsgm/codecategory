7cf45de2fb97329c7cb66f10a3a1502ec573493e
Igor Moraes <igor.sgm@gmail.com>
2017-12-04 03:14:27 -0200

    Model Category added

--- src/CodeCategory/Models/Category.php
+++ src/CodeCategory/Models/Category.php
@@ -1,15 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: igorsgm
- * Date: 04/12/2017
- * Time: 03:11
- */
 
 namespace CodePress\CodeCategory\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Category
+class Category extends Model
 {
+	protected $table = "codepress_categories";
+
+	protected $fillable = [
+		'name',
+		'active',
+		'parent_id'
+	];
 
 }
